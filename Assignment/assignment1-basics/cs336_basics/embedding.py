56ce6d398d267e0a4c5bfc4f52057c58588e9f8d
mungermiao <1875706036@qq.com>
2026-04-21 20:10:13 +0800

    upload embedding.py

--- Assignment/assignment1-basics/cs336_basics/embedding.py
+++ Assignment/assignment1-basics/cs336_basics/embedding.py
@@ -1,10 +1,21 @@
 import torch
 import torch.nn as nn
 
+from cs336_basics.linear import Linear
+
 class Embedding(nn.Module):
-    def __init__(self, num_embeddings, enbedding_dim, device=None, dtype=None):
-        pass
+    def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
+        super().__init__()
+        self.nun_embeddings = num_embeddings
+        self.d_model = embedding_dim
+        
+        self.weigths = nn.Parameter(
+            torch.empty(self.nun_embeddings,self.d_model,device=device,dtype=dtype)
+        )
+        
+        nn.init.trunc_normal_(self.weigths, mean=0.0, std=1,a = -3,b = 3)
+        
         
     
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        pass
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        return self.weigths[token_ids]
--- Assignment/assignment1-basics/cs336_basics/linear.py
+++ Assignment/assignment1-basics/cs336_basics/linear.py
@@ -1,14 +1,21 @@
 import torch
+from torch._higher_order_ops.out_dtype import out_dtype_fake_tensor_mode
 import torch.nn as nn
 import math
 
+"""
+    Linear层的作用:
+        把一个vector的特征重新加权求和,映射到另一个特征空间中去
+"""
 
 class Linear(nn.Module):
-    """核心: 1.维护一个weight参数 2.forward 过程计算矩阵乘法"""
+    """核心: 1.维护一个weight参数 2.forward 过程计算矩阵乘法"""        
     def __init__(self, in_features, out_features, device=None, dtype=None):
+        super().__init__()
         self.in_features = in_features
         self.out_features = out_features
         
+        # weigth : [d_dou,d_in]
         self.weight = nn.Parameter(
             torch.empty(out_features,in_features,device=device,dtype=dtype)
         )
@@ -16,5 +23,12 @@ class Linear(nn.Module):
         std = math.sqrt(2.0 / (in_features + out_features))
         nn.init.trunc_normal_(self.weight, mean = 0.0, std=std, a=-3 * std, b=3 * std)
     
+    
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x @ weights.T
+        # x : [batch_size, seq_len, d_in]
+        # self.weight.transpose(-1,-2) : [d_in, d_out]
+        # [batch_size, seq_len, d_in] @ [d_in, d_out] -> [batch_size, seq_len, d_out]
         return x @ self.weight.transpose(-1,-2)
+
+        
